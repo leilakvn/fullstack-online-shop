860845bca149d467a34d1f54e147c84e282f1e1c
leila <leila.kvn.isf@gmail.com>
2023-05-17 17:47:04 +0430

    revert "tsconfig"

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -5,39 +5,43 @@ import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { cartActions } from "../redux/slices/CartSlice";
 import { toast } from "react-toastify";
-const ProductCard = ({ item }) => {
+import { Product } from "../model";
+interface Props {
+  product: Product;
+}
+const ProductCard: React.FC<Props> = ({ product }) => {
   const dispatch = useDispatch();
   const addToCart = () => {
     dispatch(
       cartActions.addItem({
-        id: item.id,
-        productName: item.productName,
-        price: item.price,
-        imgUrl: item.imgUrl,
+        id: product.id,
+        productName: product.productName,
+        price: product.price,
+        imgUrl: product.imgUrl,
       })
     );
     toast.success("product added successfully");
   };
   return (
-    <Col lg="3"  sm="6" className="mb-2">
+    <Col lg="3" sm="6" className="mb-2">
       <div className="product__item">
         <div className="product__img">
           <motion.img
             whileHover={{ scale: 1.1 }}
             width="300px"
-            src={item.imgUrl}
+            src={product.imgUrl}
             alt="prodduct"
           />
         </div>
         <div className="p-2 product__info ">
           <h3 className="product__name">
-            <Link to={`/shop/${item.id}`}>{item.productName}</Link>
+            <Link to={`/shop/${product.id}`}>{product.productName}</Link>
           </h3>
-          <span>{item.category}</span>
+          <span>{product.category}</span>
         </div>
 
         <div className="product_card-bottom d-flex align-items-center justify-content-between p-2">
-          <span className="price">{item.price}</span>
+          <span className="price">{product.price}</span>
           <motion.span whileTap={{ scale: 1.2 }} onClick={addToCart}>
             <i className="ri-add-line"></i>
           </motion.span>
--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,8 @@ import store from "./redux/store";
 import { Provider } from "react-redux";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const rootElement=document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement!);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
